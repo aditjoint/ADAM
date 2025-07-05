a300431b2aa4af42fbbad7f8ae72441c1255a151
Lalitendu Samantaray <aditjoint@gmail.com>
2025-07-05 17:01:32 +0530

    Update imgProtect.js
    Signed-off-by: Lalitendu Samantaray <aditjoint@gmail.com>

--- js/imgProtect.js
+++ js/imgProtect.js
@@ -1,1 +1,19 @@
+// imgProtect.js
+document.addEventListener("DOMContentLoaded", function () {
+  const images = document.querySelectorAll("img");
+
+  images.forEach(img => {
+    // Disable right-click on images
+    img.oncontextmenu = () => false;
+
+    // Disable drag
+    img.setAttribute("draggable", "false");
+
+    // Prevent selection
+    img.style.userSelect = "none";
+    img.style.webkitUserSelect = "none";
+    img.style.msUserSelect = "none";
+    img.style.pointerEvents = "none"; // If image is not clickable
+  });
+});
 
