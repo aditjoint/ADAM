47ca4b25b3a35985f5ae31d29e5b1397488a2bde
Lalitendu Samantaray <aditjoint@gmail.com>
2025-08-31 06:56:07 +0530

    Update background.js
    Signed-off-by: Lalitendu Samantaray <aditjoint@gmail.com>

--- js/background.js
+++ js/background.js
@@ -1,9 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
   const today = new Date();
-  const day = today.getDate();
-  const imageFolder = "/images/daily-backgrounds/";
-  const imageName = `bg${day}.jpg`;
-
-  document.body.classList.add(`day${day}`); // adds CSS class instead of inline
-  document.body.style.backgroundImage = `url('${imageFolder}${imageName}')`;
+  const day = today.getDate(); // 1–31
+  document.body.classList.add(`day${day}`);
 });
