868e6485e39584772e9c89670ce014b9b3616a04
Lalitendu Samantaray <aditjoint@gmail.com>
2025-08-31 07:42:04 +0530

    Update background.js
    Signed-off-by: Lalitendu Samantaray <aditjoint@gmail.com>

--- js/background.js
+++ js/background.js
@@ -1,5 +1,3 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const today = new Date();
-  const day = today.getDate(); // 1–31
-  document.body.classList.add(`day${day}`);
+  document.body.classList.add(`day${new Date().getDate()}`);
 });
